5b475883700923cdd082e9eaafb19f0d47bc36e7
Artem Pelenitsyn <a.pelenitsyn@gmail.com>
2026-09-23 13:34:21 -0400

    some comments

--- src/Stripe.java
+++ src/Stripe.java
@@ -4,7 +4,11 @@ int stripeWidth(int u) {
 }
 
 void main() {
+
+    // checks for stripeWidth
     Check.is(stripeWidth(10), 60);
     Check.is(stripeWidth(5), 30);
+
+    // print final report at the very end:
     Check.done();
 }
